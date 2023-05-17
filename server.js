75a107b9f0ee012df4a8d62b8731da2b01ec2502
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-05-17 16:56:42 +0200

    Ajout de commentaires sur les modules utilisés

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// Module express permet de créer un serveur web
 const express = require('express');
 const app = express();
+// Module path permet de gérer les chemins de fichiers et de répertoires
 const path = require('path');
+//  Module cheerio permet d'analyser et extraire les balises souhaitées.
 const cheerio = require('cheerio');
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +16,7 @@ app.post('/endpoint', async (req, res) => {
   
   try {
     // Récupère le contenu HTML de la page web
+    // Module node-fetch permet de faire des requêtes HTTP
     const fetch = await import('node-fetch');
     const response = await fetch.default(variableFromClient);
     const html = await response.text();
